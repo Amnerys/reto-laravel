be38ad5fd2f98a414019b8367e7eabc9b33cc6fb
Amnerys <44168432+Amnerys@users.noreply.github.com>
2022-03-26 18:54:10 +0100

    Added register functionality to UserController

--- crud-api-rest/app/Http/Controllers/UserController.php
+++ crud-api-rest/app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -11,7 +12,66 @@ class UserController extends Controller
     }
 
     public function register(Request $request){
-        return "Acción de registro de Usuario";
+        //Recoger los datos del usuario por post
+        $json = $request->input('json',null);
+        $params = json_decode($json); //objeto
+        $params_array = json_decode($json,true); //array
+
+        //Si encuentra datos en el objeto y el array, continuar
+        if(!empty($params) && !empty($params_array)){
+
+            //Limpiar datos obtenidos
+            $params_array = array_map('trim', $params_array);
+
+            //Regla de validación para los datos recogidos
+            $validate = \Validator::make($params_array, [
+                'nombre'                => 'required|alpha',
+                'apellidos'             => 'required|alpha',
+                'fecha_nacimiento'      => 'date',
+                'email'                 => 'required|email|unique:users',
+                'password'              => 'required',
+                'foto'                  => 'file'
+            ]);
+
+            //Si la validación falla:
+            if($validate->fails()){
+                $data = array(
+                    'status' => 'error',
+                    'code' => '404',
+                    'message' => 'El usuario no se ha podido crear',
+                    'errors' => $validate->errors()
+                );
+            }else{ //Si la validación es CORRECTA:
+                //Cifrar la contraseña 4 veces
+                $pwd = password_hash($params->password, PASSWORD_BCRYPT, ['cost'=>4]);
+
+                //Crear el usuario y guardarlo en la base de datos
+                $user = new User();
+                $user->nombre = $params_array['nombre'];
+                $user->apellidos = $params_array['apellidos'];
+                $user->fecha_nacimiento = $params_array['fecha_nacimiento'];
+                $user->email = $params_array['email'];
+                $user->password = $pwd;
+                $user->foto = $params_array['foto'];
+                $user->save();
+
+                $data = array(
+                    'status' => 'success',
+                    'code' => '200',
+                    'message' => 'El usuario se ha creado correctamente',
+                    'user' => $user
+                );
+            }
+        }else{ //Si los datos introducidos no son correctos
+            $data = array(
+                'status' => 'success',
+                'code' => '404',
+                'message' => 'Los datos enviados no son válidos'
+            );
+        }
+
+        //Devuelve la respuesta en función si ha validado correctamente o no el usuario
+        return response()->json($data, $data['code']);
     }
 
     public function login(Request $request){
