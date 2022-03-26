3a9041ac84d9629dc817266857351aed9a96a32e
Amnerys <44168432+Amnerys@users.noreply.github.com>
2022-03-26 17:41:33 +0100

    Created Controllers
    For Category, User and Product

--- crud-api-rest/routes/web.php
+++ crud-api-rest/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PruebasController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Rutas prueba
+/*
 Route::get('/', function () {
     return '<h1>Hola Mundo!<h1>';
 });
@@ -20,3 +23,26 @@ Route::get('/', function () {
 Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/pruebas{nombre?}', function($nombre=null) {
+    $texto = '<h2>Texto desde una ruta<h2>';
+    $texto .= 'Nombre: '. $nombre;
+    return view('pruebas', array(
+        'texto' => $texto
+    ));
+});
+
+Route::get('/animales', 'App\Http\Controllers\PruebasController@index');
+Route::get('/test-orm', 'App\Http\Controllers\PruebasController@testOrm');
+*/
+
+//Rutas API (PUT actualizar recursos o datos, DELETE para eliminar datos o recursos)
+
+//Rutas de prueba GET para conseguir datos
+Route::get('/usuario/pruebas', 'App\Http\Controllers\UserController@pruebas');
+Route::get('/categoria/pruebas', 'App\Http\Controllers\CategoryController@pruebas');
+Route::get('/producto/pruebas', 'App\Http\Controllers\ProductController@pruebas');
+
+//Rutas del controlador de usuarios POST para guardar datos o hacer lógica, recibir desde formulario
+Route::post('/api/register', 'App\Http\Controllers\UserController@register');
+Route::post('/api/login', 'App\Http\Controllers\UserController@login');
