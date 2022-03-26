7af389ffb4b93021d1596f03b10b096ccbc61f18
Amnerys <44168432+Amnerys@users.noreply.github.com>
2022-03-26 13:06:53 +0100

    Added relations between tables

--- crud-api-rest/app/Models/Category.php
+++ crud-api-rest/app/Models/Category.php
@@ -9,6 +9,14 @@ class Category extends Model
 {
     use HasFactory;
 
+    //Este modelo usará la tabla 'categories'
+    protected $table = 'categories';
+
+    //Le indicamos que una categoría puede tener varios productos y que saque los productos relacionados con la categoría
+    public function productos(){
+        return $this->hasMany('App\Product');
+    }
+
     protected $fillable = [
         'nombre_categoria',
         'descripcion_categoria',
